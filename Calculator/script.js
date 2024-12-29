4f876cc6e7278526b4264f03c3b53c3d4f738f55
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2024-12-29 16:09:20 +0800

    calculator with eval method

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,10 +1,60 @@
 
 const btns = document.querySelectorAll(".btn");
+const digitsReg = /^\d+$/;
+const inoutPanel = document.querySelector("#in-out-panel");
+let digitString = "";
 
-const click = () =>{
-    alert("button clicked");
+
+inoutPanel.innerHTML = "0";
+function checkValue(value){
+    if(value == "AC"){
+        inoutPanel.innerHTML = "0";
+        digitString = "";
+    }
+    else if(value != "="){
+        digitString += value;
+        inoutPanel.innerHTML = digitString;
+    }
+    else if(value == "="){
+        digitString = eval(digitString);
+        inoutPanel.innerHTML = digitString;
+        if(digitString != ""){
+            digitString = "";
+        }
+    }
 }
 
 for (const btn of btns) {
-    btn.addEventListener('click', click);
+    btn.addEventListener('click', () => checkValue(btn.value));
 }
+
+// let digitsArray = [];
+// let optArray = [];
+// let total = 0;
+
+//    if(digitsReg.test(value)){
+//         digitString += value;
+//    }
+//    else if(value == "+" || value == "-" || value == "x" || value == "/")
+//    {
+//         digitsArray.push(digitString);
+//         if(value == "+")
+//             optArray.push("+");
+//         else if(value == "-")
+//             optArray.push("-");
+//         else if(value == "x")
+//             optArray.push("x");
+//         else if(value == "/")
+//             optArray.push("/");
+//         alert(digitsArray);
+//         alert(optArray);
+//         digitString = "";
+//    }
+//    else if(value == "=")
+//    {
+//     if(total === 0){
+//         first = digitsArray.shift();
+        
+//     }
+//     alert("will be totaled up");
+//    }
